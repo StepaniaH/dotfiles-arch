98773f5c67f8b602f9df69d317470ddd792513e4
StepaniaH <StepaniaH0907@gmail.com>
2025-02-11 00:30:20 +0800

    Update the configuration of bufferline.nvim

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -12,6 +12,17 @@ return {
         options = {
           mode = "buffers",
           style_preset = bufferline.style_preset.default, -- or bufferline.style_preset.minimal,
+          themable = true,
+          numbers = 'ordinal',
+          close_command = "bdelete! %d",
+          right_mouse_command = false,
+          left_mouse_command = "buffer %d",
+          middle_mouse_command = nil,
+          buffer_close_icon = '󰅖',
+          modified_icon = '● ',
+          close_icon = ' ',
+          left_trunc_marker = ' ',
+          right_trunc_marker = ' ',
         }
       })
     end
