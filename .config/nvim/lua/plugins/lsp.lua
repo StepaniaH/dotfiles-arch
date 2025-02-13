f234f8c0330d9b251c2c9dc90e699098975d7958
StepaniaH <StepaniaH0907@gmail.com>
2025-02-14 00:31:34 +0800

    Add new lsp(clangd and pyright)

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -17,6 +17,8 @@ return {
     config = function()
       local capabilities = require('blink.cmp').get_lsp_capabilities()
       require("lspconfig").lua_ls.setup { capabilities = capabilities }
+      require("lspconfig").clangd.setup { capabilities = capabilities }
+      require("lspconfig").pyright.setup { capabilities = capabilities }
 
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
